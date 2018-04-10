921984a25cf7d15a877911e1d26588bb0940d04d
phat <1451120121@sv.ut.edu.vn>
2018-04-10 08:57:49 +0700

    hellowork

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink ,Link } from "react-router-dom";
 import HomePages from './../../pages/Home/HomePages';
 import womenswatch from './../../pages/WomensWatchs/womenswatch';
 import menswatch from './../../pages/MensWatchs/menswatch';
@@ -7,20 +7,56 @@ import videochannel from './../../pages/VideoChannel/VideoChannel';
 import supportcenter from './../../pages/SupportCenter/supportcenter';
 import Logo from './Logo';
 
+
+
+// const MenuLink = ({label , to , activeOnlyWhenExact}) => {
+//   return (
+//     <Route path = {to} exact ={activeOnlyWhenExact} children = {({match}) =>{
+//       var active = match ? 'active' : '';
+//       return (
+//         <li className = {active}>
+//             <Link to={to} >
+//                   {label}
+//             </Link>
+//         </li>
+//       )
+//     }} 
+//     />
+//   )
+// }
 const Header = () => (
   <Router>
-  <div>
-        <div className="container">
-          <div className="row">
+  <div className="headerHover">
+        <div className="container ">
+          <div className="row ">
            <Logo />
-           <div className="col-sm-9  mt-3">
+           <div className="col-sm-9 menu  mt-3">
             <nav id="navbar">
               <ul>
-                <li><Link to="/" title="Home">Trang Chủ</Link></li>
-                <li><Link to="/donghonam" title="DongHoNam">Đồng Hồ Nam</Link></li>
-                <li><Link to="/donghonu" title="DongHoNu">Đồng Hồ Nữ</Link></li>
-                <li><Link to="/video" title="Video-Channel">Video-Channel</Link></li>
-                <li><Link to="/support" title="Support-Center">Support-Center</Link></li>
+                <li><NavLink 
+                      exact to="/" 
+                      title="Home"
+                      activeStyle = {{color:'#e65100'}}>Trang Chủ</NavLink></li>
+                {/* <MenuLink label="Trang Chủ" to= "/" activeOnlyWhenExact={true} /> */}
+                <li><NavLink 
+                      to="/donghonam" 
+                      title="DongHoNam"
+                      activeStyle = {{color:'#e65100'}}>Đồng Hồ Nam</NavLink></li>
+                <li><NavLink
+                      to="/donghonu" 
+                      title="DongHoNu"
+                      activeStyle = {{color:'#e65100'}}
+                      >Đồng Hồ Nữ</NavLink></li>
+                <li><NavLink
+                      to="/video" 
+                      title="Video-Channel"
+                      activeStyle = {{color:'#e65100'}}
+                      >Video-Channel</NavLink></li>
+                <li><NavLink
+                      to="/support" 
+                      title="Support-Center"
+                      activeStyle = {{color:'#e65100'}}
+                      >Support-Center</NavLink></li>
               </ul>
             </nav>
           </div>
